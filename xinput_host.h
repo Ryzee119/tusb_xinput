3e8c92cc6279b1cda04ade7decf14c3346741da7
Ryan Wendland <wendland@live.com.au>
2024-01-03 15:05:07 +1030

    Comment all functions

--- xinput_host.h
+++ xinput_host.h
@@ -81,13 +81,104 @@ typedef struct
 
 extern usbh_class_driver_t const usbh_xinput_driver;
 
+/**
+ * @brief Callback function called when a report is received from an XInput device.
+ *
+ * This function is called when a report is received from the specified XInput device.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @param report Pointer to the received report data.
+ * @param len Length of the received report data.
+ */
 void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len);
+
+/**
+ * @brief Weak callback function called when a report is sent to an XInput device.
+ *
+ * This function is a weakly bound callback called when a report is sent to the specified XInput device.
+ * Implementing this function is optional, and the weak attribute allows it to be overridden if needed.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @param report Pointer to the sent report data.
+ * @param len Length of the sent report data.
+ */
 TU_ATTR_WEAK void tuh_xinput_report_sent_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len);
+
+/**
+ * @brief Weak callback function called when an XInput device is unmounted.
+ *
+ * This function is a weakly bound callback called when an XInput device is unmounted.
+ * Implementing this function is optional, and the weak attribute allows it to be overridden if needed.
+ *
+ * @param dev_addr Device address of the unmounted XInput device.
+ * @param instance Instance of the unmounted XInput device.
+ */
 TU_ATTR_WEAK void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance);
+
+/**
+ * @brief Weak callback function called when an XInput device is mounted.
+ *
+ * This function is a weakly bound callback called when an XInput device is mounted.
+ * Implementing this function is optional, and the weak attribute allows it to be overridden if needed.
+ *
+ * @param dev_addr Device address of the mounted XInput device.
+ * @param instance Instance of the mounted XInput device.
+ * @param xinput_itf Pointer to the XInput interface information.
+ */
 TU_ATTR_WEAK void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_interface_t *xinput_itf);
+
+/**
+ * @brief Receive a report from an XInput device.
+ *
+ * This function attempts to receive a report from the specified XInput device.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @return True if the report is received successfully, false otherwise.
+ */
 bool tuh_xinput_receive_report(uint8_t dev_addr, uint8_t instance);
+
+/**
+ * @brief Send a report to an XInput device.
+ *
+ * This function attempts to send a report to the specified XInput device. This is a non blocking function.
+ * tuh_xinput_report_sent_cb() is called when the transfer is completed.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @param txbuf Pointer to the data to be sent.
+ * @param len Length of the data to be sent.
+ * @return True if the report is sent successfully, false otherwise.
+ */
 bool tuh_xinput_send_report(uint8_t dev_addr, uint8_t instance, const uint8_t *txbuf, uint16_t len);
+
+/**
+ * @brief Set LED status on an XInput device. (Applicated to Xbox 360 controllers only)
+ *
+ * This function sets the LED status on the specified XInput device for the specified quadrant.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @param quadrant Quadrant of the LED to set.
+ * @param block Indicates whether the operation should be blocking.
+ * @return True if LED status is set successfully, false otherwise.
+ */
 bool tuh_xinput_set_led(uint8_t dev_addr, uint8_t instance, uint8_t quadrant, bool block);
+
+/**
+ * @brief Set rumble values on an XInput device.
+ *
+ * This function sets the rumble values on the specified XInput device for left and right motors.
+ *
+ * @param dev_addr Device address of the XInput device.
+ * @param instance Instance of the XInput device.
+ * @param lValue Intensity of the left motor rumble (0 to 255)
+ * @param rValue Intensity of the right motor rumble. (0 to 255)
+ * @param block Indicates whether the operation should be blocking.
+ * @return True if rumble values are set successfully, false otherwise.
+ */
 bool tuh_xinput_set_rumble(uint8_t dev_addr, uint8_t instance, uint8_t lValue, uint8_t rValue, bool block);
 
 #ifdef __cplusplus
